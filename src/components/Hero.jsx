0aad00169678c9e9d3a2321964505bd014de81c0
Swanand <swanandkulkarni1812@gmail.com>
2024-08-29 17:27:26 +0530

    First commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Button from "./Button";
+import GoldGym from "../assets/GoldGym.png";
 
 export default function Hero() {
   return (
     <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
+      <div>
+        <img src={GoldGym} alt="Gold Gym" className="w-24 h-auto" />
+      </div>
+
       <div className="flex flex-col gap-4">
         <p>IT'S TIME TO GET</p>
         <h1 className="upppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
